c87810e5056de5a590e27513156c9f36834fa0e2
James Etchells <etch.rush@gmail.com>
2023-01-24 14:33:21 -0800

    Set up mongoDB connection

---
+++ src/pages/api/list.js
@@ -0,0 +1,16 @@
+import { connectToDatabase } from "./../../../lib/mongodb";
+
+export default async function handler(request, response) {
+
+    const { database } = await connectToDatabase();
+    const collection = database.collection(process.env.NEXT_ATLAS_COLLECTION);
+
+    const results = await collection.find({}).project({
+        "grades": 0,
+        "borough": 0,
+        "restaurant_id": 0
+    })
+        .limit(10).toArray();
+
+    response.status(200).json(results);
+} 
