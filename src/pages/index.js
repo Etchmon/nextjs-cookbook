03753795fe82cca74aa3f2a57c4eedcc798b970a
James Etchells <etch.rush@gmail.com>
2023-01-26 10:13:13 -0800

    Edit import strings

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
+import styles from '../styles/Home.module.css'
 import { useEffect, useState } from 'react';
 
 /* eslint react/no-string-refs: 0 */
