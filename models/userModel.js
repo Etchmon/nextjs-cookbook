b105941f5e0a299eb69b0f7bc00ae165483b5202
James Etchells <etch.rush@gmail.com>
2023-01-31 09:31:17 -0800

    ADD user model

---
+++ models/userModel.js
@@ -0,0 +1,10 @@
+import { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true }
+});
+
+const User = models.User || model('User', userSchema);
+
+export default User;
