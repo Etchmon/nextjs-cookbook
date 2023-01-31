538582399ea8c4d780becf9ce7d18d6b48c4fd4c
James Etchells <etch.rush@gmail.com>
2023-01-31 12:33:55 -0800

    ADD post method to add.js for creating user

--- src/pages/api/user/add.js
+++ src/pages/api/user/add.js
@@ -1,4 +1,5 @@
 import clientPromise from '../../../../lib/mongodb';
+import User from '../../../../models/userModel';
 
 
 /**
@@ -8,11 +9,28 @@ import clientPromise from '../../../../lib/mongodb';
 
 export default async function addUser(req, res) {
 
-    try {
-        const MongoClient = await clientPromise;
-    } catch (e) {
-        console.log(e);
-    }
+    if (req.method === 'POST') {
+        // Process a POST request
+        try {
 
+            const MongoClient = await clientPromise;
+            const db = MongoClient.db('CBD');
+            const collection = db.collection("Users");
+
+            const user = new User({
+                username: req.body.username,
+                password: req.body.password,
+            });
+
+            const result = await collection.insertOne(user);
+
+            return res.json({ msg: 'user created ', user });
+
+        } catch (e) {
+            console.log(e);
+        }
+    } else {
+        return res.json('NOT A POST METHOD');
+    }
 
 }
