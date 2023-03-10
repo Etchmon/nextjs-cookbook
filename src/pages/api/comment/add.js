6e013aff3f0ae24e701119f2f3e58fb26f99c389
James Etchells <etch.rush@gmail.com>
2023-03-10 09:59:34 -0800

    new file:   src/pages/api/comment/add.js 	renamed:    src/pages/api/user/signup.js -> src/pages/api/comment/delete.js 	new file:   src/pages/api/cookbook/add.js 	new file:   src/pages/api/cookbook/delete.js 	new file:   src/pages/api/recipe/add.js 	new file:   src/pages/api/recipe/delete.js

---
+++ src/pages/api/comment/add.js
@@ -0,0 +1,8 @@
+import clientPromise from '../../../../lib/mongodb';
+import User from '../../../../models/userModel';
+
+
+/**
+ * @param {import('next').NextApiRequest} req 
+ * @param {import('next').NextApiResponse} res 
+ */
