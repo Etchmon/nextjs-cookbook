6461a60652dabaa8f289e217ab8672135f1aa24f
James Etchells <etch.rush@gmail.com>
2023-03-13 12:11:29 -0700

    modified:   src/pages/api/cookbook/add.js

--- src/pages/api/cookbook/add.js
+++ src/pages/api/cookbook/add.js
@@ -1,8 +1,38 @@
-// import clientPromise from '../../../../lib/mongodb';
-// import User from '../../../../models/userModel';
+import clientPromise from '../../../../lib/mongodb';
+import Cookbook from '../../../../models/cookbookModel';
 
 
-// /**
-//  * @param {import('next').NextApiRequest} req 
-//  * @param {import('next').NextApiResponse} res 
-//  */
+/**
+ * @param {import('next').NextApiRequest} req 
+ * @param {import('next').NextApiResponse} res 
+ */
+
+export default async function addCookbook(req, res) {
+    const session = await getSession({ req });
+
+    if (req.method === 'POST') {
+        try {
+            // Connect to database
+            const MongoClient = await clientPromise;
+            const db = await MongoClient.db("CBD");
+            const collection = await db.collection("Cookbooks");
+            const userCollection = await db.collection("Users");
+            const userData = await userCollection.findOne({ email: session.user.email });
+
+            // Create new cookbook model
+            const cookbook = new Cookbook({
+                title: req.body.title,
+                description: req.body.description
+            });
+
+            // Add cookbook to cookbook collection and cookbook id to users cookbooks
+            const result = await db.collection("Cookbooks").insertOne(cookbook);
+            const userUpdate = await userData.cookbooks.myBooks.push(cookbook.id);
+
+            // Return success
+            return res.json({ msg: 'cookbook created ', cookbook });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+}
