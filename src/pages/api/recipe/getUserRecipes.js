8b4f9e2bf706ef1ef2023b4f9a22b6f021ff3203
James Etchells <etch.rush@gmail.com>
2023-03-16 13:41:45 -0700

    modified:   src/pages/api/recipe/getUserRecipes.js

--- src/pages/api/recipe/getUserRecipes.js
+++ src/pages/api/recipe/getUserRecipes.js
@@ -11,13 +11,11 @@ import { ObjectID } from 'bson';
 
 export default async function myRecipes(req, res) {
     const session = await getSession({ req });
-    console.log(session);
 
     if (req.method === 'GET') {
         // Process a GET request
         const myRecipes = [];
         const sessionRecipes = session.user.cookbooks.allRecipes;
-        console.log(sessionRecipes[1])
 
         try {
 
@@ -25,20 +23,28 @@ export default async function myRecipes(req, res) {
             const db = await MongoClient.db("CBD");
             const collection = await db.collection("Recipes");
 
-            const result = await collection.findOne({ _id: ObjectID(sessionRecipes[1]) });
-            console.log(result);
+            // const result = await collection.findOne({ _id: ObjectID(sessionRecipes[1]) });
+
+            for (const recipe of sessionRecipes) {
+                const result = await collection.findOne({ _id: ObjectID(recipe) })
+                myRecipes.push(result);
+            };
 
             // sessionRecipes.forEach(async recipe => {
-            //     console.log(recipe);
-
+            //     const result = await collection.findOne({ _id: ObjectID(recipe) });
+            //     console.log(result);
             //     myRecipes.push(result);
+            //     console.log(myRecipes)
+            //     return result;
             // });
 
+            console.log(myRecipes);
             res.status(200).json(myRecipes);
-
 
         } catch (e) {
             console.log(e);
-        }
+        };
+
+
     }
 }
