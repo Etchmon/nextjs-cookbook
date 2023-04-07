db54e3a03120756c23419a7d28a4902e3cd3102c
James Etchells <etch.rush@gmail.com>
2023-04-07 12:57:06 -0700

    modified:   src/pages/cookbook-edit.js 	modified:   src/pages/cookbooks.js 	modified:   src/pages/recipe-view.js 	modified:   src/pages/dashboard.js

---
+++ src/pages/cookbook-edit.js
@@ -0,0 +1,16 @@
+import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react';
+import clientPromise from '../../lib/mongodb';
+import { useSession, signIn, signOut, getSession } from "next-auth/react";
+import Link from 'next/link';
+
+export default function CookbookEdit() {
+
+    const { data: session, status } = useSession();
+
+    return (
+        <main>
+
+        </main>
+    )
+}
