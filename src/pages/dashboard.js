de5fe95ba7d78ac7eac26277b9662d10248e2e27
James Etchells <etch.rush@gmail.com>
2023-04-07 11:55:57 -0700

    modified:   src/pages/dashboard.js

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,2 +1,14 @@
-// Possible rework of dashboard page
-// Create modals instead of more views.
+import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react';
+import clientPromise from '../../lib/mongodb';
+import { useSession, signIn, signOut, getSession } from "next-auth/react";
+import Link from 'next/link';
+
+export default function Dashboard() {
+
+    const { data: session, status } = useSession();
+
+
+}
+
+// Redirect to Login page if User is not signed in.
