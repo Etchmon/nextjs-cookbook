95f33fddc76631e634c71d89e94582af676c1330
James Etchells <etch.rush@gmail.com>
2023-05-03 12:27:58 -0700

    modified:   src/pages/api/auth/[...nextauth].js

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -3,12 +3,15 @@ import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import clientPromise from "../../../../lib/mongodb";
 
+// define the authentication options
 const options = {
     providers: [
+        // use Google OAuth provider
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
+        // use custom email and password provider
         CredentialsProvider({
             name: "Email and Password",
             credentials: {
@@ -21,16 +24,18 @@ const options = {
                     const db = MongoClient.db('CBD');
                     const collection = db.collection("Users");
 
+                    // find the user by email
                     const userData = await collection.findOne({ email: credentials.email.toLowerCase() });
                     if (userData === null) {
-                        return null;
+                        return null; // user not found, return null
                     }
 
+                    // compare the password with the hashed password stored in the database
                     if (credentials.password != userData.password) {
-                        return null;
+                        return null; // password doesn't match, return null
                     }
 
-
+                    // user found and password matched, return the user object
                     return {
                         id: userData._id,
                         email: userData.email,
@@ -44,22 +49,26 @@ const options = {
         }),
     ],
     callbacks: {
+        // run when the user signs in
         async signIn({ user, account, profile, email, credentials }) {
             try {
                 const MongoClient = await clientPromise;
                 const db = MongoClient.db('CBD');
                 const collection = db.collection("Users");
 
+                // find the user by email
                 const userData = await collection.findOne({ email: user.email.toLowerCase() });
                 if (userData === null) {
-                    return false;
+                    return false; // user not found, return false
                 }
 
+                // user found, return true
                 return true;
             } catch (e) {
                 console.log(e);
             }
         },
+        // run when the user's session is created
         session: async (session) => {
             if (!session) return;
 
@@ -68,8 +77,10 @@ const options = {
                 const db = await MongoClient.db('CBD');
                 const collection = await db.collection("Users");
 
+                // find the user by email
                 const userData = await collection.findOne({ email: session.session.user.email });
                 if (userData === null) {
+                    // user not found, return a new user object with the email
                     return {
                         newUser: {
                             email: session.session.user.email
@@ -77,6 +88,7 @@ const options = {
                     }
                 }
 
+                // user found, return the user object
                 return {
                     user: {
                         id: userData._id,
