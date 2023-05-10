0b2792634d31d9a36f669f5ff127c18325dbe908
James Etchells <etch.rush@gmail.com>
2023-05-10 15:17:04 -0700

    modified:   src/components/dash.js 	modified:   src/pages/cookbook-add.js 	modified:   src/styles/globals.css

--- src/components/dash.js
+++ src/components/dash.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import Link from 'next/link';
+import React from 'react'
 
 const DashboardContent = ({ session, recipes, cookbooks }) => {
     return (
@@ -6,17 +7,21 @@ const DashboardContent = ({ session, recipes, cookbooks }) => {
             <h1 className="text-4xl font-bold mb-8">Welcome, {session.user.username}!</h1>
             <p className="text-xl mb-8">We hope you're having a great day.</p>
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                <div className="bg-green-200 p-4 rounded-lg shadow hover:bg-green-100">
-                    <h2 className="text-lg font-semibold mb-2">Recipes</h2>
-                    <p className="text-green-800">Total: {recipes.length}</p>
-                    <p className="text-green-800">New today: 20</p>
-                </div>
-                <div className="bg-green-200 p-4 rounded-lg shadow hover:bg-green-100">
-                    <h2 className="text-lg font-semibold mb-2">Cookbooks</h2>
-                    <p className="text-green-800">Total: {cookbooks.length}</p>
-                    <p className="text-green-800">New today: 10</p>
-                </div>
-                <div className="bg-green-200 p-4 rounded-lg shadow hover:bg-green-100">
+                <Link href="/recipe-add">
+                    <div className="bg-green-200 p-4 rounded-lg shadow hover:bg-green-100 cursor-pointer">
+                        <h2 className="text-lg font-semibold mb-2">Create Recipe</h2>
+                        <p className="text-green-800">Total: {recipes.length}</p>
+                        <p className="text-green-800">New today: 20</p>
+                    </div>
+                </Link>
+                <Link href="/cookbook-add">
+                    <div className="bg-green-200 p-4 rounded-lg shadow hover:bg-green-100 cursor-pointer">
+                        <h2 className="text-lg font-semibold mb-2">Create Cookbook</h2>
+                        <p className="text-green-800">Total: {cookbooks.length}</p>
+                        <p className="text-green-800">New today: 10</p>
+                    </div>
+                </Link>
+                <div className="bg-green-200 p-4 rounded-lg shadow hover:bg-green-100 cursor-pointer">
                     <h2 className="text-lg font-semibold mb-2">Total</h2>
                     <p className="text-green-800">Total: {(recipes.length + cookbooks.length)}</p>
                     <p className="text-green-800">New today: $200</p>
--- src/pages/cookbook-add.js
+++ src/pages/cookbook-add.js
@@ -2,6 +2,8 @@ import styles from '../styles/Home.module.css'
 import React, { useEffect, useState } from "react";
 import { useSession, signIn } from "next-auth/react";
 import { useRouter } from 'next/router';
+import Nav from '../components/navbar';
+import Footer from '../components/footer';
 
 const RecipeList = ({ data, addToBook }) => {
 
@@ -86,24 +88,50 @@ const CookbookAdd = () => {
     } else {
 
         return (
-            <div>
-                <form>
-                    <h1>Create a Coobook</h1>
-                    <label>
-                        Cookbook name:
-                        <input type="text" name="title" value={value.title} onChange={handleChange} />
-                    </label>
-                    <label>
-                        Description:
-                        <input type="textarea" name="description" value={value.description} onChange={handleChange} />
-                    </label>
+            <div class="bg-gray-900 h-screen text-gray-300 flex flex-row">
+                <Nav />
 
-                    <button onClick={handleSubmit}>Submit</button>
-                </form>
-                <div className={styles.grid}>
-                    {myRecipes && <RecipeList data={myRecipes} addToBook={addToBook} />}
+                <div class="flex-1 flex flex-col items-center justify-center px-4 sm:px-0 mt-4">
+                    <form class="w-full sm:w-1/2 mx-auto">
+                        <h1 class="text-3xl font-bold mb-4">Create a Cookbook</h1>
+
+                        <label class="block mb-4">
+                            <span class="text-lg font-semibold">Cookbook name:</span>
+                            <input type="text" name="title" class="block w-full bg-gray-800 border-gray-600 border-2 py-2 px-4 rounded-lg mt-2 text-gray-300" value={value.title} onChange={handleChange} />
+                        </label>
+
+                        <label class="block mb-4">
+                            <span class="text-lg font-semibold">Description:</span>
+                            <textarea name="description" class="block w-full bg-gray-800 border-gray-600 border-2 py-2 px-4 rounded-lg mt-2 text-gray-300" value={value.description} onChange={handleChange} />
+                        </label>
+
+                        <button class="bg-green-600 text-white py-2 px-4 rounded-lg hover:bg-green-500 w-full sm:w-auto" onClick={handleSubmit}>Submit</button>
+                    </form>
                 </div>
+
+                <div class="flex-1 flex flex-wrap overflow-hidden mt-8 justify-center items-center">
+                    <div class="h-full w-full sm:w-1/2 overflow-y-auto max-h-half-screen pr-4">
+                        <h2 class="text-2xl font-bold mb-4">Recipes Added to Book</h2>
+
+                        <ul class="list-disc pl-8">
+                            {recipes.map((recipe) => (
+                                <li key={recipe.id} class="mb-2">{recipe.title}</li>
+                            ))}
+                        </ul>
+                    </div>
+
+                    <div class="w-full sm:w-1/2 overflow-y-auto max-h-half-screen pl-4 mt-8 sm:mt-0">
+                        <h2 class="text-2xl font-bold mb-4">My Recipes</h2>
+
+                        {myRecipes && <RecipeList data={myRecipes} addToBook={addToBook} />}
+                    </div>
+                </div>
+
+                <Footer />
             </div>
+
+
+
         )
     }
 };
