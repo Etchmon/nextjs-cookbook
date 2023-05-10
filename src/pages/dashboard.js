471b5b4277203411c018d42a3cc4fb52ef9bddf3
James Etchells <etch.rush@gmail.com>
2023-05-10 09:57:43 -0700

    modified:   src/pages/dashboard.js

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -5,10 +5,13 @@ import { signOut, getSession, useSession } from "next-auth/react";
 const Dashboard = () => {
     const { data: session, status } = useSession();
 
-
     if (status === 'loading') {
         return <div>Loading...</div>;
     }
+
+    const user = session.user;
+    const recipes = user.cookbooks.allRecipes;
+    const cookbooks = user.cookbooks.myBooks;
     console.log(session)
     return (
         <div className="flex flex-col sm:flex-row min-h-screen bg-gray-800 text-gray-300">
@@ -49,18 +52,18 @@ const Dashboard = () => {
                 <p className="text-xl mb-8">We hope you're having a great day.</p>
                 <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                     <div className="bg-green-200 p-4 rounded-lg shadow hover:bg-green-100">
-                        <h2 className="text-lg font-semibold mb-2">Users</h2>
-                        <p className="text-green-800">Total: 1000</p>
+                        <h2 className="text-lg font-semibold mb-2">Recipes</h2>
+                        <p className="text-green-800">Total: {recipes.length}</p>
                         <p className="text-green-800">New today: 20</p>
                     </div>
                     <div className="bg-green-200 p-4 rounded-lg shadow hover:bg-green-100">
-                        <h2 className="text-lg font-semibold mb-2">Orders</h2>
-                        <p className="text-green-800">Total: 500</p>
+                        <h2 className="text-lg font-semibold mb-2">Cookbooks</h2>
+                        <p className="text-green-800">Total: {cookbooks.length}</p>
                         <p className="text-green-800">New today: 10</p>
                     </div>
                     <div className="bg-green-200 p-4 rounded-lg shadow hover:bg-green-100">
-                        <h2 className="text-lg font-semibold mb-2">Revenue</h2>
-                        <p className="text-green-800">Total: $10000</p>
+                        <h2 className="text-lg font-semibold mb-2">Total</h2>
+                        <p className="text-green-800">Total: {(recipes.length + cookbooks.length)}</p>
                         <p className="text-green-800">New today: $200</p>
                     </div>
                     {/* Add more items as needed */}
