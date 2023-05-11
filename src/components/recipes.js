dac17d65e9fe624b896b19a6afcfe28c418b2b9c
James Etchells <etch.rush@gmail.com>
2023-05-11 10:05:13 -0700

    modified:   src/components/recipes.js

--- src/components/recipes.js
+++ src/components/recipes.js
@@ -1,23 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { signOut, getSession, useSession } from "next-auth/react";
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const RecipeList = ({ recipes }) => {
-    console.log(recipes)
+    const router = useRouter();
+
+    const handleClick = (recipeId) => {
+        router.push({
+            pathname: '/recipe-view',
+            query: { recipeId: recipeId }
+        })
+    };
 
     return (
         <div className="bg-gray-100 p-8 rounded shadow h-full">
             <div className="grid grid-cols-1 gap-4 mb-auto flex-1 h-full overflow-y-auto pb-80">
                 {recipes.map((recipe) => (
-                    <div key={recipe.id} className="bg-green-200 p-4 rounded-lg shadow hover:bg-green-100">
-                        <h2 className="text-lg font-semibold mb-2">{recipe.title}</h2>
+                    <div
+                        key={recipe._id}
+                        className="bg-green-200 p-4 rounded-lg shadow hover:bg-green-100 cursor-pointer"
+                        onClick={() => handleClick(recipe._id)}
+                    >
+                        <h2 className="text-green-800 font-semibold mb-2">{recipe.title}</h2>
                         <p className="text-green-800">{recipe.description}</p>
                     </div>
                 ))}
             </div>
         </div>
-
-
-
     );
 };
 
