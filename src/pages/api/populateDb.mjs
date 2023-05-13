dc2c2c60aee2f44508386e9bb0ce5c8e8bc4376d
James Etchells <etch.rush@gmail.com>
2023-05-13 13:13:47 -0700

    modified:   package-lock.json 	modified:   package.json 	new file:   src/pages/api/apiRecipes.js 	new file:   src/pages/api/populateDb.mjs

---
+++ src/pages/api/populateDb.mjs
@@ -0,0 +1,23 @@
+import fetch from "node-fetch"
+
+async function populateRecipes() {
+    try {
+        const response = await fetch(`https://api.edamam.com/search?q=popular&app_id=4f61f8f6&app_key=
+c5237c5510ccd52bb9b88d3db2c9d52b&to=100`);
+
+        const data = await response.json();
+        console.log(data.hits[0].recipe.ingredients)
+
+        if (response.ok) {
+            return data
+        } else {
+            throw new Error(data.message || 'Failed to fetch recipes.');
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+        return null;
+    }
+}
+
+// Call the function to populate the recipes
+populateRecipes();
