b4c9ab127148c70925dac1805a366d9101d54b6e
James Etchells <etch.rush@gmail.com>
2023-05-13 19:08:25 -0700

    modified:   src/pages/api/populateDb.mjs

--- src/pages/api/populateDb.mjs
+++ src/pages/api/populateDb.mjs
@@ -6,7 +6,16 @@ async function populateRecipes() {
 c5237c5510ccd52bb9b88d3db2c9d52b&to=100`);
 
         const data = await response.json();
-        console.log(data.hits[0].recipe.ingredients)
+        console.log(data.hits[0].recipe.label)
+        const promises = data.hits.map((recipe) => {
+            const recipeNew = {
+                title: recipe.recipe.label,
+                ingredients: recipe.recipe.ingredients
+            };
+            console.log(recipeNew);
+        });
+        const results = await Promise.all(promises);
+        console.log(await results)
 
         if (response.ok) {
             return data
