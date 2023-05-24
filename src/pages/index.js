3c82e0f0384c3cedca2a8b7a48c2699be2768669
James Etchells <etch.rush@gmail.com>
2023-05-24 11:03:49 -0700

    modified:   src/pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,20 +18,27 @@ export default function Home() {
   // Hook to access the Next.js session object
   const { data: session, status } = useSession();
   const [imageLoaded, setImageLoaded] = useState(false);
+  const [showContent, setShowContent] = useState(false);
 
   const handleImageLoad = () => {
     setImageLoaded(true);
   };
 
+  useEffect(() => {
+    if (imageLoaded) {
+      setTimeout(() => {
+        setShowContent(true);
+      }, 300); // Delay showing the content to allow the fade-in effect to work
+    }
+  }, [imageLoaded]);
+
 
   // Show loading indicator while the session is being checked
-  if (status === 'loading') {
-    return <Loading />
-  }
 
   // Render the page
   return (
     <div className="min-h-screen flex flex-col">
+      {!imageLoaded && <Loading />}
       <Head>
         <title>CookBook Digital</title>
         <meta name="description" content="Organize your recipes and plan your dinners with CookBook Digital." />
@@ -39,26 +46,30 @@ export default function Home() {
       <Navbar />
 
       <main className="bg-gray-800 text-gray-800 flex flex-grow">
-        {!imageLoaded && <Loading />}
         <Image
           src={bgImage}
           alt="Background Image"
           onLoadingComplete={handleImageLoad}
           quality={100} // Adjust image quality if needed
-          className="absolute inset-0 filter blur-md backdrop-filter backdrop-blur-lg backdrop-opacity-10 w-full h-full object-cover z-0"
+          className={`absolute inset-0 ${imageLoaded ? 'opacity-100' : 'opacity-0' // Apply opacity based on image loaded state
+            } transition-opacity duration-500 ease-in-out filter blur-md backdrop-filter backdrop-blur-lg backdrop-opacity-10 w-full h-full object-cover z-0`}
         />
         <div className="absolute inset-0 bg-gray-900 opacity-50"></div>
-        <section className="flex flex-col items-center justify-center w-full z-10">
-          <div className="max-w-4xl mx-auto px-4">
-            <div className=" flex flex-col items-center justify-center text-white">
-              <h1 className="text-5xl font-bold text-center mb-8 text-green-500">Organize your recipes and plan your dinners</h1>
-              <p className="text-xl text-center mb-16">CookBook Digital is the only cookbook you'll ever need.</p>
-              <Link href="/signup" className="bg-green-500 text-gray-800 py-2 px-4 rounded hover:bg-green-600">
-                Get started
-              </Link>
+        {imageLoaded &&
+          <section className="flex flex-col items-center justify-center w-full z-10">
+            <div className={`max-w-4xl mx-auto px-4 ${showContent ? 'opacity-100 transition-opacity duration-1000' : 'opacity-0'
+              }`}>
+              <div className="flex flex-col items-center justify-center text-white">
+                <h1 className={`text-5xl font-bold text-center mb-8 text-green-500 ${imageLoaded ? 'opacity-100' : 'opacity-0' // Apply opacity based on image loaded state
+                  } transition-opacity duration-1000 ease-in-out`}>Organize your recipes and plan your dinners</h1>
+                <p className="text-xl text-center mb-16">CookBook Digital is the only cookbook you'll ever need.</p>
+                <Link href="/signup" className="bg-green-500 text-gray-800 py-2 px-4 rounded hover:bg-green-600">
+                  Get started
+                </Link>
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
+        }
         <Footer />
       </main>
     </div>
