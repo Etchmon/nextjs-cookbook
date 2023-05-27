abbc07478ca3c35d0ba29f6c5a64cef87bb2d776
James Etchells <etch.rush@gmail.com>
2023-05-26 17:12:28 -0700

    modified:   src/components/footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="py-4 px-8 fixed bottom-0 w-full">
             <div className="container mx-auto">
-                <div className="flex flex-col md:flex-row justify-between items-center">
+                <div className="flex flex-col md:flex-row justify-center items-center">
                     <p className="text-lg text-green-500">&copy; 2023 CookBook Digital. All rights reserved.</p>
                 </div>
             </div>
